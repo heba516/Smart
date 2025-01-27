3f9baf33215dd4ac61e6dc5c3a7131c7c294610d
Heba Yasser <hebay99@gmail.com>
2025-01-27 05:11:14 +0200

    update root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,8 @@ import type { Metadata } from "next";
 import { Urbanist } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
-
+import SessionProvider from "@/providers/SessionProvider";
+import { getServerSession } from "next-auth";
 
 const urbanist = Urbanist({
   subsets: ["latin"],
@@ -14,38 +15,47 @@ export const metadata: Metadata = {
   title: "Smart Mart",
   description: "",
   icons: {
-    icon: "/logo.png",
+    icon: "/images/logo.png",
+  },
+  openGraph: {
+    title: "Smart mart",
+    description: "Smart mart",
+    images: {
+      url: "/images/logo.png",
+    },
   },
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const session = await getServerSession();
   return (
     <html lang="en">
-      <body className={`${urbanist.variable} antialiased`}>{children}
+      <body className={`${urbanist.variable} antialiased`}>
+        <SessionProvider session={session}>{children}</SessionProvider>
         <Toaster
-                position="top-center"
-                gutter={12}
-                containerStyle={{ margin: "8px" }}
-                toastOptions={{
-                    success: {
-                        duration: 3000,
-                    },
-                    error: {
-                        duration: 5000,
-                    },
-                    style: {
-                        fontSize: "16px",
-                        maxWidth: "500px",
-                        padding: "16px 24px",
-                        backgroundColor: "white",
-                        color: "black",
-                    },
-                }}
-            />
+          position="top-center"
+          gutter={12}
+          containerStyle={{ margin: "8px" }}
+          toastOptions={{
+            success: {
+              duration: 3000,
+            },
+            error: {
+              duration: 5000,
+            },
+            style: {
+              fontSize: "16px",
+              maxInlineSize: "500px",
+              padding: "16px 24px",
+              backgroundColor: "white",
+              color: "black",
+            },
+          }}
+        />
       </body>
     </html>
   );
