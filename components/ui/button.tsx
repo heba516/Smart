db3e766c428893f0d72994ab18c665083babffcb
Heba Yasser <hebay99@gmail.com>
2025-01-27 23:21:58 +0200

    update shadcn/ui btn component

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -12,8 +12,7 @@ const buttonVariants = cva(
         default: "bg-primary text-primary-foreground",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
-        outline:
-          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
+        outline: "border border-input bg-background",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
