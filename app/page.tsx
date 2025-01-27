bc3edd27d9930c2935f0823150a68c1afbdbc465
Heba Yasser <hebay99@gmail.com>
2025-01-27 23:22:31 +0200

    update home page

--- app/page.tsx
+++ app/page.tsx
@@ -4,16 +4,19 @@ import Image from "next/image";
 import Link from "next/link";
 import Heading from "@/components/Heading";
 import OfferBar from "@/components/OfferBar";
+import Landing from "@/components/Landing";
 
 export default function Home() {
   return (
     <div>
-      <OfferBar />
-      <NavBar />
+      <section className="sticky top-0 w-full z-50">
+        <OfferBar />
+        <NavBar />
+      </section>
 
       <Link
         href={""}
-        className="fixed right-0 top-1/4 w-fit py-2 px-3 rounded-l-full bg-white"
+        className="fixed right-0 top-1/4 w-fit py-2 px-3 rounded-l-full bg-white z-50"
       >
         <Image
           src={"/images/scanicon.png"}
@@ -23,8 +26,10 @@ export default function Home() {
         />
       </Link>
 
+      <Landing />
+
       {/* Features */}
-      <div className="mx-auto text-center">
+      <div className="min-h-screen place-content-center mx-auto text-center py-10">
         <Heading
           title="Features in Smart"
           desc="Lorem ipsum dolor sit amet consectetur. Sed elementum eget nascetur
