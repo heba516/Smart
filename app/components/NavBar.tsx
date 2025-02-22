279b00ad1a53f122bfdfaaaeb2e68ba3e544e66a
Heba Yasser <hebay99@gmail.com>
2025-02-22 03:01:18 +0200

    add updates in navbar

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -62,17 +62,10 @@ const NavBar = () => {
     setIsLoggedIn(!!Cookies.get("token"));
   }, []);
 
-  if (isLoggedIn === null) {
-    return <p>Loading</p>;
-  }
-
   const getUserName = Cookies.get("userName");
   const userName = isLoggedIn && getUserName ? JSON.parse(getUserName) : null;
 
   const isAuthenticated = session || isLoggedIn;
-
-  console.log({ isLoggedIn });
-  console.log({ userName });
 
   const logoutFun = () => {
     if (isLoggedIn) {
@@ -97,160 +90,164 @@ const NavBar = () => {
         ))}
       </ul>
 
-      <section className="flex items-center space-x-2 md:space-x-[22px]">
-        {isAuthenticated ? (
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <div className="flex items-center space-x-2 font-semibold text-lg md:text-xl">
-                <span className="bg-primaryRed p-2 md:p-3 rounded-full text-white uppercase cursor-pointer">
-                  {isLoggedIn
-                    ? userNameSplitFun(userName)
-                    : userNameSplitFun(session?.user?.name)}
-                </span>
-                <p className="xl:flex items-center hidden cursor-pointer">
-                  Welcome,{" "}
-                  {userName?.split(" ")[0] ||
-                    session?.user?.name?.split(" ")[0]}{" "}
-                  <Image
-                    className="ml-2"
-                    alt="smart"
-                    src={"/images/downarrow.svg"}
-                    width={10}
-                    height={10}
-                  />
-                </p>
-              </div>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent className="w-56">
-              {DropDownMenu.map((itm) => (
-                <DropdownMenuItem key={itm.label}>
-                  <Link className="w-full" href={itm.href}>
-                    {itm.label}
-                  </Link>
+      {isLoggedIn === null ? (
+        <></>
+      ) : (
+        <section className="flex items-center space-x-2 md:space-x-[22px]">
+          {isAuthenticated ? (
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <div className="flex items-center space-x-2 font-semibold text-lg md:text-xl">
+                  <span className="bg-primaryRed p-2 md:p-3 rounded-full text-white uppercase cursor-pointer">
+                    {isLoggedIn
+                      ? userNameSplitFun(userName)
+                      : userNameSplitFun(session?.user?.name)}
+                  </span>
+                  <p className="xl:flex items-center hidden cursor-pointer">
+                    Welcome,{" "}
+                    {userName?.split(" ")[0] ||
+                      session?.user?.name?.split(" ")[0]}{" "}
+                    <Image
+                      className="ml-2"
+                      alt="smart"
+                      src={"/images/downarrow.svg"}
+                      width={10}
+                      height={10}
+                    />
+                  </p>
+                </div>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="w-56">
+                {DropDownMenu.map((itm) => (
+                  <DropdownMenuItem key={itm.label}>
+                    <Link className="w-full" href={itm.href}>
+                      {itm.label}
+                    </Link>
+                  </DropdownMenuItem>
+                ))}
+
+                <DropdownMenuItem>
+                  <span className="w-full cursor-pointer" onClick={logoutFun}>
+                    Logout
+                  </span>
                 </DropdownMenuItem>
-              ))}
+              </DropdownMenuContent>
+            </DropdownMenu>
+          ) : (
+            <div className="xl:block space-x-[14px] hidden">
+              <Button
+                asChild
+                size={"lg"}
+                className="bg-primaryRed hover:bg-secondaryRed rounded-lg w-32 duration-300"
+              >
+                <Link href={"/signup"} className="font-semibold text-xl">
+                  Sign up
+                </Link>
+              </Button>
+              <Button
+                asChild
+                variant={"outline"}
+                size={"lg"}
+                className="hover:border-primaryRed hover:bg-transparent rounded-lg w-32 hover:text-primaryRed duration-300"
+              >
+                <Link href={"/login"} className="font-semibold text-xl">
+                  Login
+                </Link>
+              </Button>
+            </div>
+          )}
 
-              <DropdownMenuItem>
-                <span className="w-full cursor-pointer" onClick={logoutFun}>
-                  Logout
-                </span>
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
-        ) : (
-          <div className="xl:block space-x-[14px] hidden">
-            <Button
-              asChild
-              size={"lg"}
-              className="bg-primaryRed hover:bg-secondaryRed rounded-lg w-32 duration-300"
-            >
-              <Link href={"/signup"} className="font-semibold text-xl">
-                Sign up
+          <div className="xl:flex space-x-3 hidden">
+            {Icons.map((icon, index) => (
+              <Link
+                href={session ? `${icon.href}` : `/login`}
+                key={index}
+                className="border-medGray hover:border-primaryRed p-[9px] border rounded-full w-11 h-11 duration-300 cursor-pointer"
+              >
+                <Image
+                  src={icon.label}
+                  alt="smart"
+                  width={25}
+                  height={25}
+                  className="cursor-pointer"
+                />
               </Link>
-            </Button>
-            <Button
-              asChild
-              variant={"outline"}
-              size={"lg"}
-              className="hover:border-primaryRed hover:bg-transparent rounded-lg w-32 hover:text-primaryRed duration-300"
-            >
-              <Link href={"/login"} className="font-semibold text-xl">
-                Login
-              </Link>
-            </Button>
+            ))}
           </div>
-        )}
 
-        <div className="xl:flex space-x-3 hidden">
-          {Icons.map((icon, index) => (
-            <Link
-              href={session ? `${icon.href}` : `/login`}
-              key={index}
-              className="border-medGray hover:border-primaryRed p-[9px] border rounded-full w-11 h-11 duration-300 cursor-pointer"
-            >
+          <Sheet>
+            <SheetTrigger asChild>
               <Image
-                src={icon.label}
+                src={"/images/menu.svg"}
                 alt="smart"
-                width={25}
-                height={25}
-                className="cursor-pointer"
+                width={51}
+                height={51}
+                className="xl:hidden w-10 md:w-[51px] h-10 md:h-[51px] cursor-pointer"
+                loading="lazy"
               />
-            </Link>
-          ))}
-        </div>
+            </SheetTrigger>
+            <SheetTitle></SheetTitle>
+            <SheetContent side="top" className="h-screen text-center">
+              <SheetHeader>
+                <SmartLogo />
+              </SheetHeader>
 
-        <Sheet>
-          <SheetTrigger asChild>
-            <Image
-              src={"/images/menu.svg"}
-              alt="smart"
-              width={51}
-              height={51}
-              className="xl:hidden w-10 md:w-[51px] h-10 md:h-[51px] cursor-pointer"
-              loading="lazy"
-            />
-          </SheetTrigger>
-          <SheetTitle></SheetTitle>
-          <SheetContent side="top" className="h-screen text-center">
-            <SheetHeader>
-              <SmartLogo />
-            </SheetHeader>
-
-            <ul className="space-y-9 mx-auto py-9 border-b border-lightGray w-fit">
-              {NavLinks.map((link, index) => (
-                <li
-                  key={index}
-                  className="font-medium text-xl hover:text-primaryRed leading-6 duration-300"
-                >
-                  <SheetClose asChild>
-                    <Link href={link.href}>{link.label}</Link>
-                  </SheetClose>
-                </li>
-              ))}
-            </ul>
-            {!isAuthenticated && (
-              <section className="space-y-9 mx-auto py-9 border-b border-lightGray">
-                <p className="font-normal text-base text-darkGray">
-                  Want to benefit from a personalized experience and access
-                  exclusive content?
-                </p>
-
-                <div className="space-x-5">
-                  <Button
-                    size={"lg"}
-                    className="bg-primaryRed hover:bg-secondaryRed rounded-lg w-32 duration-300"
+              <ul className="space-y-9 mx-auto py-9 border-b border-lightGray w-fit">
+                {NavLinks.map((link, index) => (
+                  <li
+                    key={index}
+                    className="font-medium text-xl hover:text-primaryRed leading-6 duration-300"
                   >
-                    <Link href={"/signup"} className="font-semibold text-xl">
-                      Sign up
-                    </Link>
-                  </Button>
-                  <Button
-                    variant={"outline"}
-                    size={"lg"}
-                    className="hover:border-primaryRed hover:bg-transparent rounded-lg w-32 hover:text-primaryRed duration-300"
-                  >
-                    <Link href={"/login"} className="font-semibold text-xl">
-                      Login
-                    </Link>
-                  </Button>
-                </div>
+                    <SheetClose asChild>
+                      <Link href={link.href}>{link.label}</Link>
+                    </SheetClose>
+                  </li>
+                ))}
+              </ul>
+              {!isAuthenticated && (
+                <section className="space-y-9 mx-auto py-9 border-b border-lightGray">
+                  <p className="font-normal text-base text-darkGray">
+                    Want to benefit from a personalized experience and access
+                    exclusive content?
+                  </p>
 
-                <div className="flex space-x-5 mx-auto w-fit">
-                  {Icons.map((icon, index) => (
-                    <Link
-                      href={icon.href}
-                      key={index}
-                      className="border-medGray hover:border-primaryRed p-[9px] border rounded-full w-11 h-11 hover:text-primaryRed duration-300 cursor-pointer"
+                  <div className="space-x-5">
+                    <Button
+                      size={"lg"}
+                      className="bg-primaryRed hover:bg-secondaryRed rounded-lg w-32 duration-300"
                     >
-                      <Icon icon={icon.label} width="25" height="26" />
-                    </Link>
-                  ))}
-                </div>
-              </section>
-            )}
-          </SheetContent>
-        </Sheet>
-      </section>
+                      <Link href={"/signup"} className="font-semibold text-xl">
+                        Sign up
+                      </Link>
+                    </Button>
+                    <Button
+                      variant={"outline"}
+                      size={"lg"}
+                      className="hover:border-primaryRed hover:bg-transparent rounded-lg w-32 hover:text-primaryRed duration-300"
+                    >
+                      <Link href={"/login"} className="font-semibold text-xl">
+                        Login
+                      </Link>
+                    </Button>
+                  </div>
+
+                  <div className="flex space-x-5 mx-auto w-fit">
+                    {Icons.map((icon, index) => (
+                      <Link
+                        href={icon.href}
+                        key={index}
+                        className="border-medGray hover:border-primaryRed p-[9px] border rounded-full w-11 h-11 hover:text-primaryRed duration-300 cursor-pointer"
+                      >
+                        <Icon icon={icon.label} width="25" height="26" />
+                      </Link>
+                    ))}
+                  </div>
+                </section>
+              )}
+            </SheetContent>
+          </Sheet>
+        </section>
+      )}
     </nav>
   );
 };
