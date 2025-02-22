28eb1d59d5df3aa21d85ac41f5ebabe04072c067
Heba Yasser <hebay99@gmail.com>
2025-02-22 03:03:11 +0200

    update base url

--- utils/axiosInstance.ts
+++ utils/axiosInstance.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const AxiosInstance = axios.create({
-    baseURL: "https://faint-ilyse-iot-based-smart-retail-system-897f175c.koyeb.app/api/auth/"
+    baseURL: "https://faint-ilyse-iot-based-smart-retail-system-897f175c.koyeb.app/api/"
 });
