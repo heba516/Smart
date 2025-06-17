ceace74e382eb631f6b01db592711370451365fe
Heba Yasser <hebay99@gmail.com>
2025-06-18 00:32:05 +0300

    add sales chart

--- app/(admin)/dashboard/sales/page.tsx
+++ app/(admin)/dashboard/sales/page.tsx
@@ -1,7 +1,11 @@
-import React from "react";
-
+"use client"
+import FormHeader from "../inventory/[action]/_components/FormHeader";
+import { SalesLineChart } from "./components/SalesLineChart";
 const page = () => {
-  return <div>page</div>;
+  return <div className="py-5 space-y-10">
+    <FormHeader page={"sales"} />
+    <SalesLineChart />
+  </div>;
 };
 
 export default page;
