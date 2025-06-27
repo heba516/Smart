2f1ae8ef94c8b859c7652da71ea2da27985a9c24
Heba Yasser <hebay99@gmail.com>
2025-06-27 22:41:11 +0300

    enhance sidebar on hover style

--- components/ui/sidebar.tsx
+++ components/ui/sidebar.tsx
@@ -525,11 +525,11 @@ const SidebarMenuItem = React.forwardRef<
 SidebarMenuItem.displayName = "SidebarMenuItem";
 
 const sidebarMenuButtonVariants = cva(
-  "peer/menu-button font-semibold flex w-full text-center items-center gap-2 overflow-hidden rounded-xl p-2 px-4 outline-none ring-sidebar-ring transition-[width,height,padding] hover:bg-primaryRed hover:text-white focus-visible:ring-2 active:bg-primaryRed active:text-white disabled:pointer-events-none disabled:opacity-50 group-has-[[data-sidebar=menu-action]]/menu-item:pr-8 aria-disabled:pointer-events-none aria-disabled:opacity-50 data-[active=true]:bg-primaryRed data-[active=true]:font-medium data-[active=true]:text-white data-[state=open]:hover:bg-primaryRed data-[state=open]:hover:text-white group-data-[collapsible=icon]:!size-8 group-data-[collapsible=icon]:!p-2 [&>span:last-child]:truncate [&>svg]:size-7 [&>svg]:shrink-0",
+  "peer/menu-button font-semibold flex w-full text-center items-center gap-2 overflow-hidden rounded-xl p-2 px-4 outline-none ring-sidebar-ring transition-[width,height,padding] hover:text-primaryRed  focus-visible:ring-2 active:bg-primaryRed active:text-white disabled:pointer-events-none disabled:opacity-50 group-has-[[data-sidebar=menu-action]]/menu-item:pr-8 aria-disabled:pointer-events-none aria-disabled:opacity-50 data-[active=true]:bg-primaryRed data-[active=true]:font-medium data-[active=true]:text-white data-[state=open]:hover:text-primaryRed data-[state=open]: group-data-[collapsible=icon]:!size-8 group-data-[collapsible=icon]:!p-2 [&>span:last-child]:truncate [&>svg]:size-7 [&>svg]:shrink-0",
   {
     variants: {
       variant: {
-        default: "hover:bg-primaryRed hover:text-white text-grayColor",
+        default: "hover:text-primaryRed  text-grayColor",
         outline:
           "bg-background shadow-[0_0_0_1px_hsl(var(--sidebar-border))] hover:bg-sidebar-accent hover:text-sidebar-accent-foreground hover:shadow-[0_0_0_1px_hsl(var(--sidebar-accent))]",
       },
@@ -619,7 +619,7 @@ const SidebarMenuAction = React.forwardRef<
       ref={ref}
       data-sidebar="menu-action"
       className={cn(
-        "absolute right-1 top-1.5 flex aspect-square w-5 items-center justify-center rounded-md p-0 text-grayColor outline-none ring-sidebar-ring transition-transform hover:bg-primaryRed hover:text-white focus-visible:ring-2 peer-hover/menu-button:text-sidebar-accent-foreground [&>svg]:size-8 [&>svg]:shrink-0",
+        "absolute right-1 top-1.5 flex aspect-square w-5 items-center justify-center rounded-md p-0 text-grayColor outline-none ring-sidebar-ring transition-transform hover:text-primaryRed  focus-visible:ring-2 peer-hover/menu-button:text-sidebar-accent-foreground [&>svg]:size-8 [&>svg]:shrink-0",
         // Increases the hit area of the button on mobile.
         "after:absolute after:-inset-2 after:md:hidden",
         "peer-data-[size=sm]/menu-button:top-1",
@@ -735,7 +735,7 @@ const SidebarMenuSubButton = React.forwardRef<
       data-size={size}
       data-active={isActive}
       className={cn(
-        "flex h-7 min-w-0 -translate-x-px items-center gap-2 overflow-hidden rounded-md px-2 text-sidebar-foreground outline-none ring-sidebar-ring hover:bg-primaryRed hover:text-white focus-visible:ring-2 active:bg-primaryRed active:text-white disabled:pointer-events-none disabled:opacity-50 aria-disabled:pointer-events-none aria-disabled:opacity-50 [&>span:last-child]:truncate [&>svg]:size-8 [&>svg]:shrink-0 [&>svg]:text-sidebar-accent-foreground",
+        "flex h-7 min-w-0 -translate-x-px items-center gap-2 overflow-hidden rounded-md px-2 text-sidebar-foreground outline-none ring-sidebar-ring hover:text-primaryRed  focus-visible:ring-2 active:bg-primaryRed active:text-white disabled:pointer-events-none disabled:opacity-50 aria-disabled:pointer-events-none aria-disabled:opacity-50 [&>span:last-child]:truncate [&>svg]:size-8 [&>svg]:shrink-0 [&>svg]:text-sidebar-accent-foreground",
         "data-[active=true]:bg-primaryRed data-[active=true]:text-white",
         size === "sm" && "text-xs",
         size === "md" && "text-sm",
